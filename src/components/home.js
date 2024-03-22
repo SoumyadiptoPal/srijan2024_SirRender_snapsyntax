0f52f0d9425058c6166760fcd620cd82dda98f3e
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2024-03-22 16:40:35 +0530

    Footer

---
+++ src/components/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            Hello World
+        </div>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
