a4f77f84e854f6ec029be314e9bffb95d04f0b86
SoumyadiptoPal <soumyadiptocodes@gmail.com>
2024-03-22 18:26:56 +0530

    Added all files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,68 @@
-import React from 'react'
-
+import React from "react";
+import "./Footer/styles.css";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div>
+      <div className="footer_cont">
+        <div className="left">
+          <div className="avatar">
+            <h1>JANELLE BROWN</h1>
+            <p>Senior Software Developer</p>
+          </div>
+          <div className="profile_image">
+            <img src="picturesIcon.jpeg" alt="image" height={200} />
+          </div>
+        </div>
 
-export default Footer
+        <div className="right">
+          <div className="topinfo">
+            <h1>Think I'll be a good fit for your team or project?</h1>
+            <h1>Let's connect.</h1>
+            <h2>Contact me</h2>
+            <input type="email" placeholder="info@JanelleBrown.in" />
+            <input type="submit" />
+          </div>
+
+          <div className="bottominfo">
+            <div className="projects">
+              <h3>INMPLEMENTED PROJECTS</h3>
+              <p>
+                <a href="./">Project 1</a>
+              </p>
+
+              <p>
+                <a href="./">Project 2</a>
+              </p>
+              <a href="./">Project 3</a>
+            </div>
+            <div className="socials">
+              <h3>SOCIALS</h3>
+              <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
+              <p>
+                <FaTwitter /> Twitter{" "}
+              </p>
+              <p>
+                {" "}
+                <FaLinkedin /> Linkedin{" "}
+              </p>
+              </div>
+              <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
+              <p>
+                <FaFacebook /> Faceboook 
+              </p>
+              <p>
+                <FaFacebook /> Instagram 
+              </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer_cont">
+      <div className="left">
+        <div className="avatar">
+          <h1>JANELLE BROWN</h1>
+          <p>Senior Software Developer</p>
+        </div>
+        <div className="profile_image">
+          <img src="assets/avatar.png" alt="avatar" width="100" height="100" />
+        </div>
+      </div>
 
-export default Footer
+      <div className="right">
+        <div className="topinfo">
+          <h1>Think I'll be a good fit for your team or project?</h1>
+          <h1>Let's connect.</h1>
+          <h2>Contact me</h2>
+          <input type="email" placeholder="info@JanelleBrown.in" />
+          <input type="submit" />
+        </div>
+
+        <div className="bottominfo">
+          <div className="projects">
+            <h3>INMPLEMENTED PROJECTS</h3>
+            <p>
+              <a href="./">Project 1</a>
+            </p>
+
+            <p>
+              <a href="./">Project 2</a>
+            </p>
+            <a href="./">Project 3</a>
+          </div>
+          <div className="socials">
+            <h3>SOCIALS</h3>
+            <p>
+              <img src="assets/licon.png" alt="licon" width="25" height="25" />{" "}
+              Linkedin{" "}
+              <img src="assets/ticon.png" alt="ticon" width="25" height="25" />{" "}
+              Twitter
+            </p>
+            <p>
+              <img
+                src="assets/fbicon.png"
+                alt="fbicon"
+                width="25"
+                height="25"
+              />{" "}
+              Faceboook{" "}
+              <img
+                src="assets/igicon.png"
+                alt="igicon"
+                width="25"
+                height="25"
+              />{" "}
+              Instagram
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
